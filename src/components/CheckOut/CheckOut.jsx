be0012e78768c7c0d37272cefdb6945d8ac7f39b
RodrigoERodriguez <rodrigoerodriguez48@gmail.com>
2023-11-28 05:07:58 -0300

    Se agrega validacion de correo

--- src/components/CheckOut/CheckOut.jsx
+++ src/components/CheckOut/CheckOut.jsx
@@ -1,86 +1,87 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from 'react';
 import { CartContext } from '../../context/CartContex/CartContex';
-import { useForm } from "react-hook-form";
-import { collection, addDoc} from "firebase/firestore";
-import { baseDeDatos } from "../../FireBase/config"
+import { useForm } from 'react-hook-form';
+import { collection, addDoc } from 'firebase/firestore';
+import { baseDeDatos } from '../../FireBase/config';
 
 const CheckOut = () => {
+    const [pedidoId, setPedidoId] = useState('');
+    const [ errorEmail, setErrorEmail] = useState(false);
 
-    const [pedidoId, setPedidoId] = useState("");
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, watch } = useForm();
     const { carrito, precioTotal, vaciarCarrito } = useContext(CartContext);
 
     const FechaPedido = new Date();
 
     const Compra = (data) => {
-        const pedido = {
-            estado: "Generada",
-            fecha: FechaPedido, 
-            cliente: data,
-            productos: carrito,
-            total: precioTotal()
+        if (data.email !== watch('confirmEmail')) {
+        setErrorEmail(true);
+        return;
         }
 
-        const referenciaPedido = collection(baseDeDatos, "PedidosDeCompra"); 
+        const pedido = {
+        estado: 'Generada',
+        fecha: FechaPedido,
+        cliente: data,
+        productos: carrito,
+        total: precioTotal(),
+        };
 
-        addDoc(referenciaPedido, pedido)
-            .then((doc) =>{
-                setPedidoId(doc.id);
-                vaciarCarrito();
-            })
-    }
+        const referenciaPedido = collection(baseDeDatos, 'PedidosDeCompra');
 
-    if(pedidoId){
-    return(
+        addDoc(referenciaPedido, pedido).then((doc) => {
+        setPedidoId(doc.id);
+        vaciarCarrito();
+        });
+    };
+
+    if (pedidoId) {
+        return (
         <div className="container">
             <h1 className="main-title">Gracias por tu compra</h1>
-            <h2>Tu numero de tu pedido es: {pedidoId}</h2>
-        </div>  
-        )
+            <h2>Tu número de pedido es: {pedidoId}</h2>
+        </div>
+        );
     }
 
     const cantidadTotal = carrito.reduce((total, prod) => total + prod.cantidad, 0);
 
     return (
-
         <>
-
-            <div>
-                <h1>Carrito</h1>
-                {
-                    carrito.map((prod) => (
-                        <div key={prod.id}> 
-                            <h3>{prod.nameProduct}</h3>
-                            <p>Precio total: ${prod.price * prod.cantidad}</p>
-                            <p>Cantidad: {prod.cantidad}</p>
-                        </div>
-                    ))
-                }
-
-                {   
-                    carrito.length > 0 ?
-                    <>
-                        <h2>Productos en Carrito: {cantidadTotal}</h2>
-                        <h2>Precio total: ${precioTotal()}</h2>
-                    </> :
-                    <h2>El carrito esta vacio</h2>
-                }
+        <div>
+            <h1>Carrito</h1>
+            {carrito.map((prod) => (
+            <div key={prod.id}>
+                <h3>{prod.nameProduct}</h3>
+                <p>Precio total: ${prod.price * prod.cantidad}</p>
+                <p>Cantidad: {prod.cantidad}</p>
             </div>
+            ))}
+            {carrito.length > 0 ? (
+            <>
+                <h2>Productos en Carrito: {cantidadTotal}</h2>
+                <h2>Precio total: ${precioTotal()}</h2>
+            </>
+            ) : (
+            <h2>El carrito está vacío</h2>
+            )}
+        </div>
 
-            <div className="container">
-                <h1 className="main-title">Finalizar compra</h1>
-                <form className="formulario" onSubmit={handleSubmit(Compra)}>
-
-                    <input type="text" placeholder="Ingresá tu nombre" {...register("nombre")} />
-                    <input type="email" placeholder="Ingresá tu e-mail" {...register("email")} />
-                    <input type="phone" placeholder="Ingresá tu teléfono" {...register("telefono")} />
-
-                    <button className="enviar" type="submit">Comprar</button>
-
-                </form>
-            </div>
+        <div className="container">
+            <h1 className="main-title">Finalizar compra</h1>
+            <form className="formulario" onSubmit={handleSubmit(Compra)}>
+            <input type="text" placeholder="Ingresá tu nombre" {...register('nombre')} />
+            <input type="email" placeholder="Ingresá tu e-mail" {...register('email')} />
+            <input type="email" placeholder="Confirma tu e-mail" {...register('confirmEmail')} />
+            {errorEmail && <p>Los correos electrónicos no coinciden.</p>}
+            <input type="phone" placeholder="Ingresá tu teléfono" {...register('telefono')} />
+            <button className="enviar" type="submit">
+                Comprar
+            </button>
+            </form>
+        </div>
         </>
-    )
-}
+    );
+};
 
-export default CheckOut
+export default CheckOut;
